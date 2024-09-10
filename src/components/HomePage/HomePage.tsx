56bf2236c5a0a9c62171d42e719c21f198710f8e
Maria Lapshina <mariaspiiish@gmail.com>
2024-09-10 19:12:54 +0400

    add screenHeight dependency for the scroller

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 import { topics } from '../../data/topics.json'
 import Topic from '../Topic/Topic';
 import { updateLocalStore } from '../../utils/utility-functions';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TopicList } from '../../utils/types';
 
 type PropsType = {
@@ -11,8 +11,21 @@ type PropsType = {
 
 function HomePage({ setTopic, isModalOpen }: PropsType) {
     updateLocalStore(topics);
+    const [scrollerHeight, setScrollerHeight] = useState(window.innerHeight > 750 ? '600px' : '450px');
     const [listOfTopics, setListOfTopics] = useState<TopicList>(JSON.parse(localStorage.getItem("topics") || '[{"topic": "none", "checked": false}]'))
     const uncheckedTopics = listOfTopics.filter(item => item.checked === false);
+    
+    useEffect(() => {
+        const handleResize = () => {
+            setScrollerHeight(window.innerHeight > 750 ? '600px' : '450px');
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
 
     const pageContent = (
         uncheckedTopics.map((item, i) => {
@@ -34,16 +47,18 @@ function HomePage({ setTopic, isModalOpen }: PropsType) {
     const content = (
         <main className='main'>
             <h1 className='main__header'>What topic would you like to discuss today?</h1>
-            {uncheckedTopics.length === 0 ?
-                <p className='main__apology'>You've covered all the topics we provided. We're working on adding new ones to discuss. Meanwhile you might check the discussed topics again.</p>
-                :<div className='scroller'>
-                <ul className='scroller__inner'>
-                    {pageContent}
-                </ul>
-            </div>
+            {uncheckedTopics.length === 0 ? (
+                    <p className='main__apology'>
+                        You've covered all the topics we provided. We're working on adding new ones to discuss. Meanwhile you might check the discussed topics again.
+                    </p>
+                ) : (
+                    <div className='scroller' style={{maxHeight: `${scrollerHeight}`}}>
+                        <ul className='scroller__inner'>
+                            {pageContent}
+                        </ul>
+                    </div>
+                )
             }
-            
-            
         </main>
     )
     
